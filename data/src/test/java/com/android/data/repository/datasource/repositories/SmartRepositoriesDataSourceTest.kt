21005ce878f37f6eecad3edfedbda3cee3b1a7b0
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-15 22:18:56 +0330

    Implement repositories data source unit tests

--- data/src/test/java/com/android/data/repository/datasource/repositories/SmartRepositoriesDataSourceTest.kt
+++ data/src/test/java/com/android/data/repository/datasource/repositories/SmartRepositoriesDataSourceTest.kt
@@ -1,8 +1,149 @@
 package com.android.data.repository.datasource.repositories
 
-import org.junit.Assert.*
+import com.android.common_test.IdFieldCacheKeyResolver
+import com.android.common_test.TestUtils
+import com.android.data.LoadRepositoriesQuery
+import com.android.data.entity.dao.RepositoriesDao
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy
+import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory
+import com.apollographql.apollo.fetcher.ApolloResponseFetchers
+import com.nhaarman.mockitokotlin2.*
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
 
 /**
  * Created by hassanalizadeh on 15,October,2020
  */
-class SmartRepositoriesDataSourceTest
+class SmartRepositoriesDataSourceTest {
+
+    @get:Rule
+    var server = MockWebServer()
+
+    private lateinit var apolloClient: ApolloClient
+    private lateinit var spyApolloClient: ApolloClient
+
+    @Mock
+    private lateinit var repositoriesDao: RepositoriesDao
+    private lateinit var dataSource: RepositoriesDataSource
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .dispatcher(Dispatcher(TestUtils.immediateExecutorService()))
+            .build()
+
+        apolloClient = ApolloClient.builder()
+            .serverUrl(server.url("/"))
+            .okHttpClient(okHttpClient)
+            .normalizedCache(
+                LruNormalizedCacheFactory(EvictionPolicy.NO_EVICTION),
+                IdFieldCacheKeyResolver()
+            )
+            .defaultResponseFetcher(ApolloResponseFetchers.NETWORK_ONLY)
+            .dispatcher(TestUtils.immediateExecutor())
+            .build()
+
+        spyApolloClient = spy(apolloClient)
+
+        dataSource = Mockito.spy(
+            SmartRepositoriesDataSource(
+                spyApolloClient,
+                repositoriesDao
+            )
+        )
+    }
+
+    @Test
+    fun `load repositories from DB`() {
+        // GIVEN
+        Mockito.doReturn(Flowable.just(TestUtils.loadRepositoriesFromDB()))
+            .whenever(repositoriesDao)
+            .selectAll()
+
+        // WHEN
+        dataSource.loadRepositories()
+            .test()
+            .assertComplete()
+
+        // THEN
+        Mockito.verify(repositoriesDao).selectAll()
+    }
+
+    @Test
+    fun `fetch repositories from server`() {
+        // GIVEN
+        server.enqueue(TestUtils.fetchRepositoriesFromRemote())
+
+        // WHEN
+        dataSource.fetchRepositories()
+            .test()
+            .assertComplete()
+
+        // THEN
+        verify(spyApolloClient).query<LoadRepositoriesQuery.Data, LoadRepositoriesQuery.Data, LoadRepositoriesQuery.Variables>(
+            argThat { query ->
+                query.variables().after().value == null
+            })
+        verify(repositoriesDao).insert(argThat {
+            this.size == TestUtils.repositoriesSize()
+        })
+    }
+
+    @Test
+    fun `error on fetching repositories from server`() {
+        // GIVEN
+        server.enqueue(TestUtils.errorResponse())
+
+        // WHEN
+        dataSource.fetchRepositories()
+            .test()
+            .assertNotComplete()
+
+        // THEN
+        verify(repositoriesDao, never()).insert(any())
+    }
+
+    @Test
+    fun `fetch more repositories from server`() {
+        // GIVEN
+        val after = TestUtils.getEndCursor()
+
+        server.enqueue(TestUtils.fetchRepositoriesFromRemote())
+        server.enqueue(TestUtils.fetchRepositoriesFromRemote())
+
+        // WHEN
+        dataSource.fetchRepositories()
+            .andThen(Completable.fromCallable {
+                reset(spyApolloClient)
+                reset(repositoriesDao)
+            })
+            .andThen(dataSource.fetchMoreRepositories())
+            .test()
+            .assertComplete()
+
+        // THEN
+        verify(
+            spyApolloClient
+        ).query<LoadRepositoriesQuery.Data, LoadRepositoriesQuery.Data, LoadRepositoriesQuery.Variables>(
+            argThat { query ->
+                query.variables().after().value == after
+            })
+        verify(repositoriesDao).insert(argThat {
+            this.size == TestUtils.repositoriesSize()
+        })
+    }
+
+}
