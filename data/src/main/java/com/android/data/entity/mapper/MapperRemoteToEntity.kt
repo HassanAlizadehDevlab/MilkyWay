02faa2dbb99bf8cb874dfb75cfe24cbbef8f8bb4
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-15 22:16:01 +0330

    Change query object to solve issue in Graphql

--- data/src/main/java/com/android/data/entity/mapper/MapperRemoteToEntity.kt
+++ data/src/main/java/com/android/data/entity/mapper/MapperRemoteToEntity.kt
@@ -1,33 +1,16 @@
 package com.android.data.entity.mapper
 
-import android.annotation.SuppressLint
 import com.android.data.LoadRepositoriesQuery
 import com.android.data.entity.model.local.RepositoryEntity
-import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 /**
  * Created by hassanalizadeh on 15,October,2020
  */
 fun LoadRepositoriesQuery.AsRepository.map() = RepositoryEntity(
     nameWithOwner = nameWithOwner(),
-    createdAt = DateUtil.formatDate(createdAt().toString()),
+    createdAt = createdAt().toString(),
     stargazerCount = stargazerCount(),
     forkCount = forkCount(),
     description = description().toString(),
     url = url().toString()
 )
-
-object DateUtil {
-    private const val OLD_FORMAT = "yyyy-MM-ddTHH:mm:ss"
-    private val NEW_FORMAT = "yyyy-MM-dd"
-    @SuppressLint("SimpleDateFormat")
-    private val dt = SimpleDateFormat(OLD_FORMAT)
-
-    fun formatDate(dateTime: String): String {
-        val date = dt.parse(dateTime)
-        dt.applyPattern(NEW_FORMAT)
-        return dt.format(date)
-    }
-}
