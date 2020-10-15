7c3b9fdd9f0b107bf68896d6712ee7ed2c191e60
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-16 00:25:38 +0330

    Implement Detail DataSource tests

--- data/src/test/java/com/android/data/repository/datasource/detail/SmartDetailDataSourceTest.kt
+++ data/src/test/java/com/android/data/repository/datasource/detail/SmartDetailDataSourceTest.kt
@@ -1,8 +1,58 @@
 package com.android.data.repository.datasource.detail
 
-import org.junit.Assert.*
+import com.android.common_test.TestUtils
+import com.android.data.network.DataServiceContributors
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 /**
  * Created by hassanalizadeh on 16,October,2020
  */
-class SmartDetailDataSourceTest
+@RunWith(JUnit4::class)
+class SmartDetailDataSourceTest {
+
+    @Mock
+    private lateinit var service: DataServiceContributors
+    private lateinit var smartDetailDataSource: SmartDetailDataSource
+
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        smartDetailDataSource = Mockito.spy(
+            SmartDetailDataSource(
+                service
+            )
+        )
+    }
+
+
+    @Test
+    fun `get contributors`() {
+        // GIVEN
+        val owner = "mojombo"
+        val repo = "grit"
+        val perPage = 5
+        Mockito.doReturn(Single.just(mutableListOf(TestUtils.fetchContributors())))
+            .whenever(service)
+            .getContributors(anyString(), anyString(), anyInt())
+
+        // WHEN
+        smartDetailDataSource.fetchContributors(owner, repo)
+            .test()
+            .assertComplete()
+
+        // THEN
+        Mockito.verify(service).getContributors(owner, repo, perPage)
+    }
+
+}
