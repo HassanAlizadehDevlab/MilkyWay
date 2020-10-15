623e10265bee3e2ee22af04eb637f6314495cce0
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-15 22:55:57 +0330

    Provide Completable Transformer for whoe usecases

--- domain/src/main/java/com/android/domain/DomainModule.kt
+++ domain/src/main/java/com/android/domain/DomainModule.kt
@@ -1,9 +1,17 @@
 package com.android.domain
 
+import com.android.domain.executor.transformer.AsyncCTransformer
+import com.android.domain.executor.transformer.CTransformer
+import dagger.Binds
 import dagger.Module
 
 /**
  * Created by hassanalizadeh on 19,September,2020
  */
 @Module
-abstract class DomainModule
+abstract class DomainModule {
+    @Binds
+    abstract fun completableTransformer(
+        transformer: AsyncCTransformer
+    ): CTransformer
+}
