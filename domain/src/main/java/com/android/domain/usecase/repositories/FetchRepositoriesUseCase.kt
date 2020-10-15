66608f4099bc35e5f801ce16b517df4e775df2f5
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-15 22:55:31 +0330

    Implement fetch github repositories from remote usecase

---
+++ domain/src/main/java/com/android/domain/usecase/repositories/FetchRepositoriesUseCase.kt
@@ -0,0 +1,20 @@
+package com.android.domain.usecase.repositories
+
+import com.android.domain.executor.transformer.CTransformer
+import com.android.domain.repository.GithubRepositoriesRepository
+import com.android.domain.usecase.UseCaseCompletable
+import io.reactivex.Completable
+import javax.inject.Inject
+
+/**
+ * Created by hassanalizadeh on 15,October,2020
+ */
+class FetchRepositoriesUseCase @Inject constructor(
+    private val repository: GithubRepositoriesRepository,
+    private val transformer: CTransformer
+) : UseCaseCompletable<Unit>() {
+    override fun execute(param: Unit): Completable {
+        return repository.fetchRepositories()
+            .compose(transformer)
+    }
+}
