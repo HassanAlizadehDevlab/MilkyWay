870c6cd295cc9089e5a57b6097f9376abee0f527
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-16 05:55:51 +0330

    Provide dependencies

--- data/src/main/java/com/android/data/repository/RepositoryModule.kt
+++ data/src/main/java/com/android/data/repository/RepositoryModule.kt
@@ -1,10 +1,28 @@
 package com.android.data.repository
 
 import com.android.data.repository.datasource.DataSourceModule
+import com.android.domain.repository.DetailRepository
+import com.android.domain.repository.GithubRepositoriesRepository
+import dagger.Binds
 import dagger.Module
+import dagger.Reusable
 
 /**
  * Created by hassanalizadeh on 19,September,2020
  */
 @Module(includes = [DataSourceModule::class])
-abstract class RepositoryModule
+abstract class RepositoryModule {
+
+    @Binds
+    @Reusable
+    abstract fun provideRepositoriesRepository(
+        repositoriesRepositoryImpl: GithubRepositoriesRepositoryImpl
+    ): GithubRepositoriesRepository
+
+    @Binds
+    @Reusable
+    abstract fun provideDetailRepository(
+        detailRepositoryImpl: DetailRepositoryImpl
+    ): DetailRepository
+
+}
--- data/src/main/java/com/android/data/repository/datasource/DataSourceModule.kt
+++ data/src/main/java/com/android/data/repository/datasource/DataSourceModule.kt
@@ -1,9 +1,29 @@
 package com.android.data.repository.datasource
 
+import com.android.data.repository.datasource.detail.DetailDataSource
+import com.android.data.repository.datasource.detail.SmartDetailDataSource
+import com.android.data.repository.datasource.repositories.RepositoriesDataSource
+import com.android.data.repository.datasource.repositories.SmartRepositoriesDataSource
+import dagger.Binds
 import dagger.Module
+import dagger.Reusable
 
 /**
  * Created by hassanalizadeh on 19,September,2020
  */
 @Module
-abstract class DataSourceModule
+abstract class DataSourceModule {
+
+    @Binds
+    @Reusable
+    abstract fun provideRepositoriesDataSource(
+        smartRepositoriesDataSource: SmartRepositoriesDataSource
+    ): RepositoriesDataSource
+
+    @Binds
+    @Reusable
+    abstract fun provideDetailDataSource(
+        smartDetailDataSource: SmartDetailDataSource
+    ): DetailDataSource
+
+}
