5bb7c598bedc89bcffdd17056aa79823e81bb7d5
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-16 05:22:41 +0330

    Implement repository click action

---
+++ presentation/src/main/java/com/android/presentation/ui/repositories/adapter/ViewRepositoryAction.kt
@@ -0,0 +1,8 @@
+package com.android.presentation.ui.repositories.adapter
+
+import com.android.presentation.adapter.BaseAction
+
+/**
+ * Created by hassanalizadeh on 16,October,2020
+ */
+data class ViewRepositoryAction(val data: String) : BaseAction
