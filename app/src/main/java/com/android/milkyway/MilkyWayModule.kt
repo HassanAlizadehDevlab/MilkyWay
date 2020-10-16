142b53c5caf24692aab7493226829670beb35f70
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-16 05:26:31 +0330

    Add MainActivityModule to main module

--- app/src/main/java/com/android/milkyway/MilkyWayModule.kt
+++ app/src/main/java/com/android/milkyway/MilkyWayModule.kt
@@ -1,9 +1,13 @@
 package com.android.milkyway
 
 import android.app.Application
+import com.android.presentation.common.di.ActivityScope
+import com.android.presentation.ui.MainActivity
+import com.android.presentation.ui.MainActivityModule
 import dagger.Binds
 import dagger.Module
 import dagger.android.AndroidInjectionModule
+import dagger.android.ContributesAndroidInjector
 import javax.inject.Singleton
 
 /**
@@ -15,5 +19,9 @@ abstract class MilkyWayModule {
     @Binds
     @Singleton
     abstract fun application(application: MilkyWay): Application
+
+    @ActivityScope
+    @ContributesAndroidInjector(modules = [MainActivityModule::class])
+    abstract fun mainActivityInjector(): MainActivity
 
 }
