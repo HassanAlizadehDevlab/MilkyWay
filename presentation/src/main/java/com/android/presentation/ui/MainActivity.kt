81f63e57b08c96db1337140d257e3238d21057c2
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2020-10-16 04:17:24 +0330

    Add MainActivity and its module

---
+++ presentation/src/main/java/com/android/presentation/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.android.presentation.ui
+
+import android.os.Bundle
+import com.android.presentation.R
+import com.android.presentation.common.view.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null)
+            navigator.showRepositories()
+    }
+}
---
+++ presentation/src/main/java/com/android/presentation/ui/MainActivityModule.kt
@@ -0,0 +1,31 @@
+package com.android.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.android.presentation.common.di.ActivityScope
+import com.android.presentation.common.di.FragmentScope
+import com.android.presentation.common.view.BaseActivityModule
+import com.android.presentation.ui.repositories.RepositoryFragment
+import com.android.presentation.ui.repositories.RepositoryFragmentModule
+import dagger.Binds
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+/**
+ * Created by hassanalizadeh on 16,October,2020
+ */
+@Module(
+    includes = [
+        BaseActivityModule::class
+    ]
+)
+abstract class MainActivityModule {
+
+    @FragmentScope
+    @ContributesAndroidInjector(modules = [RepositoryFragmentModule::class])
+    abstract fun repositoriesFragmentInjector(): RepositoryFragment
+
+    @Binds
+    @ActivityScope
+    abstract fun activity(mainActivity: MainActivity): AppCompatActivity
+
+}
